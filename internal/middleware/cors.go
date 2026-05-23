04858df5859f45cde8b65caeb9370b8c1119fc7e
Cagdas Korkut <cagdaskorkut@gmail.com>
2026-05-23 22:25:03 +0300

    modify cors for home id header

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,15 +1,16 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 // CORSMiddleware simple CORS middleware to handle OPTIONS
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-home-id")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
--- internal/middleware/cors_test.go
+++ internal/middleware/cors_test.go
@@ -59,7 +59,7 @@ func checkCORSHeaders(t *testing.T, w *httptest.ResponseRecorder) {
 
 	expectedHeaders := map[string]string{
 		"Access-Control-Allow-Origin":  "*",
-		"Access-Control-Allow-Headers": "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+		"Access-Control-Allow-Headers": "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-home-id",
 		"Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, PATCH, DELETE",
 	}
 
